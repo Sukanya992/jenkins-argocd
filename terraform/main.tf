7ea73654ed2964d321e70b14a98095d063e7b4cf
Sukanya992 <sukanya09200206@gmail.com>
2025-04-18 13:56:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = "plated-epigram-452709-h6"
-  region  = "us-central1"
+  zone  = "us-central1-b"
 }
 
 resource "google_container_cluster" "primary" {
   name     = "primary-cluster"
-  location = "us-central1-a"
+  location = "us-central1-b"
 
   initial_node_count = 3
 
